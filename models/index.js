4d6b231b6416a8c774727eb3cc39c2cf253d706a
Jason DeLine <jcdeline1982@gmail.com>
2023-05-22 17:30:03 -0500

    adding ProductTags.js, and updating association definitions

--- models/index.js
+++ models/index.js
@@ -1,15 +1,25 @@
 const Category = require('./category');
 const Product = require('./product');
 const Tag = require('./tag');
+const ProductTag = require('./productTags')
 
 // Define associations between models
-Category.hasMany(Product, { foreignKey: 'category_id' });
 Product.belongsTo(Category, { foreignKey: 'category_id' });
+Category.hasMany(Product, { foreignKey: 'category_id' });
+
 
 
 // Define a new one-to-many association between Tag and Product
-Tag.hasMany(Product, { foreignKey: 'tag_id' });
-Product.belongsTo(Tag, { foreignKey: 'tag_id' });
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+    foreignKey: 'product_id'
+});
+
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    foreignKey: 'tag_id'
+});
+
 
 // Export the initialized models
 module.exports = { Category, Product, Tag };
