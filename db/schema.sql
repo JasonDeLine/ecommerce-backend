48b99c27d41ee6580f3397fece173663733ff870
Jason DeLine <jcdeline1982@gmail.com>
2023-05-22 14:22:16 -0500

    adding schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,40 @@
+-- create DB
+CREATE DATABASE IF NOT EXISTS estore_db;
+USE estore_db;
+
+-- Drop the tables if they exist
+DROP TABLE IF EXISTS product_tags;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS categories;
+
+-- Create the categories table
+CREATE TABLE categories (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(50) NOT NULL
+);
+
+-- Create the tags table
+CREATE TABLE tags (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(50)
+);
+
+-- Create the products table
+CREATE TABLE products (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT,
+  FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
+);
+
+-- Create the product_tags table
+CREATE TABLE product_tags (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT,
+  tag_id INT,
+  FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
+  FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE
+);
