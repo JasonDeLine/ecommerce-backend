0ca8671ad8646b861e679bfe84cdbd425b7e8861
Jason DeLine <jcdeline1982@gmail.com>
2023-05-24 17:40:54 -0500

    updating server.js to call seedAll

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const routes = require('./routes/api');
-const sequelize = require('./config/connection.js'); // Import sequelize connection
+const sequelize = require('./config/connection.js');
+const seedAll = require('./seeds'); // Import the seed function
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,8 +12,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// Sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
+// Sync sequelize models to the database
+sequelize.sync({ force: false }).then(async () => {
+  // Seed the database
+  await seedAll();
+
+  // Start the server
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
   });
